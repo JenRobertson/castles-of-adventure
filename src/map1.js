6b642ae1e0ca67eaf1ebad1b0b839a7bbbbd39c6
Jenny ☺ <jenny@jennyrobertson.net>
2020-02-09 11:49:13 +0000

    add wall sprites

--- src/map1.js
+++ src/map1.js
@@ -1,28 +1,25 @@
 export const map1 = {
     key: {
-        0: {
-            sprite: 'blank',
-            walkable: false
-        },
-        1: {
-            sprite: 'tiles',
-            walkable: true
-        },
-        2: {
-            sprite: 'wall',
-            walkable: false
-        },
-        3: {
-            sprite: 'water',
-            walkable: false
-        },
-        4: {
-            sprite: 'sand',
-            walkable: true
-        }
+        0: { sprite: 'blank',walkable: false },
+        1: { sprite: 'tiles', walkable: true },
+
+        20: { sprite: 'wall-top-left', walkable: false , isEdge: true },
+        21: { sprite: 'wall-top-middle', walkable: false , isEdge: true },
+        22: { sprite: 'wall-top-right', walkable: false , isEdge: true },
+        23: { sprite: 'wall-middle-left', walkable: false , isEdge: true },
+        24: { sprite: 'wall-middle', walkable: false },
+        25: { sprite: 'wall-middle-right', walkable: false , isEdge: true },
+        26: { sprite: 'wall-bottom-left', walkable: false , isEdge: true },
+        27: { sprite: 'wall-bottom-middle', walkable: false , isEdge: true },
+        28: { sprite: 'wall-bottom-right', walkable: false , isEdge: true },
+        29: { sprite: 'wall-left-right', walkable: false , isEdge: true },
+        210: { sprite: 'wall-top-bottom', walkable: false , isEdge: true },
+
+        3: { sprite: 'water', walkable: false },
+        4: { sprite: 'sand',walkable: true }
     },
     data: [
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,20,0,0,0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
